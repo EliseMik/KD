b2bbf81b5e7932255b1565848ca73b77906152e9
EliseMik <90831108+EliseMik@users.noreply.github.com>
2021-09-28 12:49:40 +0300

    Add files via upload

---
+++ laba 4.cpp
@@ -0,0 +1,162 @@
+//laba 4 (1)
+/*
+
+#include <iostream>
+
+#include <cstdlib>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+	int a, b, S, P;
+
+	cin >> a >> b;
+
+	S = a * b;
+
+	P = 2 * (a + b);
+
+	cout << S << " " << P << endl;
+
+	return 0;
+
+}
+
+*/
+
+
+//laba 4 (2)
+/*
+#include <iostream>
+
+#include <cstdlib>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+   float pi = 3.14;
+
+   float d,L;
+
+   cin >> d;
+
+   L = d * pi;
+
+   cout << L << endl;
+
+   return 0;
+
+}
+
+
+*/
+
+
+//laba 4 (3)
+
+/*
+#include <iostream>
+
+#include <cstdlib>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+   float a,b,A;
+
+
+
+   cin >> a >> b;
+
+   A = (a+b)/2;
+
+   cout << A << endl;
+
+   return 0;
+
+}
+*/
+
+//laba 4 (4)
+
+/*
+#include <iostream>
+
+#include <cstdlib>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+   float a,b;
+
+
+
+   cin >> a >> b;
+
+   cout << (a * a) + (b * b) << endl;
+
+   cout << (a * a) - (b * b) << endl;
+
+   cout << (a * a) * (b * b) << endl;
+
+   cout << (a * a) / (b * b) << endl;
+
+   return 0;
+
+}
+
+
+*/
+
+
+//laba 4 (5)
+
+/*
+#include<iostream>
+
+#include<cstdlib>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+float a, b;
+
+cin >> a >> b;
+
+cout << abs(a) + abs(b) << endl;
+
+cout << abs(a) - abs(b) << endl;
+
+cout << abs(a) * abs(b) << endl;
+
+cout << abs(a) / abs(b) << endl;
+
+return 0;
+
+}
+*/
