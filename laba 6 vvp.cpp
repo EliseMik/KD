cf46f4e189f979e8f41b2001c206b0157a7254e4
EliseMik <90831108+EliseMik@users.noreply.github.com>
2021-09-30 11:21:53 +0300

    Add files via upload

---
+++ laba 6 vvp.cpp
@@ -0,0 +1,219 @@
+﻿//laba 6.1
+/*
+#include<iostream>
+
+#include<cmath>
+
+using namespace std;
+
+int main()
+
+{
+
+int a, b;
+
+cin >> a >> b;
+
+int t = a;
+
+a = b;
+
+b = t;
+
+cout << a << " " << b << endl;
+
+return 0;
+
+}
+
+
+*/
+
+
+//laba 6.2
+/*
+#include<iostream>
+
+#include<cmath>
+
+using namespace std;
+
+int main()
+
+{
+
+int a, b, c;
+
+cin >> a >> b>> c;
+
+int t = b;
+
+b = a;
+
+a = c;
+
+c = t;
+
+cout << a << " " << b << " "<< c<< endl;
+
+return 0;
+
+}
+
+
+*/
+
+
+//laba 6.3
+/*
+#include<iostream>
+
+#include<cmath>
+
+using namespace std;
+
+int main()
+
+{
+
+int C, b, a;
+
+cin >> C >> b>> a;
+
+int t = b;
+
+b = C;
+
+C = a;
+
+a = t;
+
+cout << C << " " << b << " "<< a<< endl;
+
+return 0;
+
+}
+
+
+*/
+
+
+//laba 6.4
+/*
+#include<iostream>
+
+#include<cmath>
+
+using namespace std;
+
+int main()
+
+{
+
+	int x;
+
+	cin >> x;
+
+	int y = 3 * pow(x, 6) - 6 * pow(x, 2) - 7;
+
+	cout << y << endl;
+
+	return 0;
+
+}
+
+*/
+
+
+//laba 6.5
+/*
+#include<iostream>
+
+#include<cmath>
+
+using namespace std;
+
+int main()
+
+{
+
+int x;
+
+cin >> x;
+
+x -= 3;
+
+int y = 4 * pow(x,6) - 7 * pow(x,2)+2;
+
+cout << y << endl;
+
+return 0;
+
+}
+
+
+*/
+
+
+//laba 6.6
+/*
+#include<iostream>
+
+#include<cmath>
+
+using namespace std;
+
+int main()
+
+{
+
+int x;
+
+cin >> x;
+
+x = x * x;
+
+x = x * x;
+
+x = x * x;
+
+cout << x << endl;
+
+return 0;
+
+}
+*/
+
+
+//laba 6.7
+/*
+#include<iostream>
+
+#include<cmath>
+
+using namespace std;
+
+int main()
+
+{
+
+int x,t;
+
+cin >> x;
+
+x = x * x * x;
+
+t = x;
+
+x = x * x;
+
+x = x * x;
+
+x = x * t;
+
+cout << x << endl;
+
+return 0;
+
+}
+*/
