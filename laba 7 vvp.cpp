7a714f5f99a20745faa6d29b52161eb16c49173a
EliseMik <90831108+EliseMik@users.noreply.github.com>
2021-09-30 11:31:47 +0300

    Add files via upload

---
+++ laba 7 vvp.cpp
@@ -0,0 +1,107 @@
+//laba 7 (1)
+/*
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, r;
+    cin >> a;
+    r = a / 180;
+    cout << r << "pi" << endl;
+    return 0;
+}
+*/
+
+
+//laba 7 (2)
+/*
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a,r;
+    cin >> r;
+    a = r * 180;
+    cout << a << endl;
+    return 0;
+}
+*/
+
+
+//laba 7 (3)
+/*
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float x, a, y, sum;
+    cin >> x >> a >> y;
+    sum = a / x;
+    y = sum * y;
+    cout << y << endl;
+    return 0;
+}
+*/
+
+//laba 7 (4)
+/*
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float v1, v2, S, t;
+    cin >> v1 >> v2 >> S>> t;
+    cout << S + (v1 + v2) * t << endl;
+    return 0;
+}
+*/
+
+
+//laba 7 (5)
+/*
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b;
+    cin >> a >> b;
+    cout << -b / a << endl;
+    return 0;
+}
+*/
+
+
+//laba 7 (6)
+/*
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a1, b1, c1, a2, b2, c2,x,y;
+    cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2;
+    if ((a1 * b2 - b1 * a2) == 0)
+    {
+        if (c1/c2 == a1/a2)
+        {
+            cout << "belongs R" << endl;
+        }
+        else
+        {
+            cout << "belongs to " << endl;
+        }
+    }
+    else
+    {
+        cout << -(b1 * c2 - c1 * b2) / (a1 * b2 - b1 * a2) << endl;
+        cout << (a1 * c2 - c1 * a2) / (a1 * b2 - b1 * a2) << endl;
+    }
+    return 0;
+}
+*/
+
