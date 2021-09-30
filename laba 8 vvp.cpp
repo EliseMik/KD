4f903b69bd6cc86f6cc0f0f1e5a737699b940d29
EliseMik <90831108+EliseMik@users.noreply.github.com>
2021-09-30 11:35:24 +0300

    Add files via upload

---
+++ laba 8 vvp.cpp
@@ -0,0 +1,80 @@
+﻿//laba 8
+
+//laba 8.1
+/*
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	float x;
+	int a;
+	cin >> x;
+	a = x / 1024;
+	cout << a << endl;
+	return 0;
+}
+*/
+
+//laba 8.2
+/*
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << a / b << endl;
+	return 0;
+}
+*/
+
+//laba 8.3
+/*
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << a%b << endl;
+	return 0;
+}
+*/
+
+//laba 8.4
+/*
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+	cout << a % 10 << a / 10 << endl;
+	return 0;
+}
+
+*/
+
+//laba 8.5
+/*
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+	cout << a % 100 << a / 100 << endl;
+	return 0;
+}
+*/
+
+
