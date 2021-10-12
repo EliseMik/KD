de44a5846660f546521945bc778f6cfc1d2fc2ee
EliseMik <90831108+EliseMik@users.noreply.github.com>
2021-10-12 10:53:03 +0300

    Update laba 5 vvp.cpp

--- laba 5 vvp.cpp
+++ laba 5 vvp.cpp
@@ -13,7 +13,7 @@ int main()
 
 {
 
-cout<<"Enter the coordinates of the points (х1, у1), (х2, у2)" ;
+cout<<"Enter the coordinates of the points (х1, у1), (х2, у2)"<<endl ;
 
 float x1, y1, x2, y2;
 
@@ -50,11 +50,11 @@ cout<< "Enter the coordinates of 3 points (а, b, c) on the number line OX" ;
 
 cin >> a >> b >> c;          //точки на одной прямой
 
-cout << "the distance between a and c is "; abs(a - c) << endl;         //расстояние между а и с
+cout << "the distance between a and c is "<< abs(a - c) << endl;         //расстояние между а и с
 
-cout << "the distance between b and c is "; abs(b - c) << endl;          //расстояние между в и с
+cout << "the distance between b and c is "<< abs(b - c) << endl;          //расстояние между в и с
 
-cout << "the distance between (a and c) + (c and b) is "; abs(a - c) + abs(b - c) << endl;           //сумма расстояний
+cout << "the distance between (a and c) + (c and b) is "<< abs(a - c) + abs(b - c) << endl;           //сумма расстояний
 
 return 0;
 
@@ -83,11 +83,11 @@ int main()
 
 float a, b, c;
 
-cout<< "Enter the coordinates of 3 points (а, b, c) on the number line OX" ;
+cout<< "Enter the coordinates of 3 points (а, b, c) on the number line OX"<<endl ;
 
 cin >> a >> b >> c;
 
-cout << "Multiplication of distance between (a and c) + (c and b) is "; abs(a - c) * abs(b - c) << endl;       //произведение расстояний
+cout << "Multiplication of distance between (a and c) + (c and b) is "<< abs(a - c) * abs(b - c) << endl;       //произведение расстояний
 
 return 0;
 
@@ -114,7 +114,7 @@ int main()
 
 double x1, y1, x2, y2;
 
-cout<<"Enter the coordinates of the points (х1, у1), (х2, у2) of rectangle" ;
+cout<<"Enter the coordinates of the points (х1, у1), (х2, у2) of rectangle" <<endl;
 
 cin >> x1 >> y1 >> x2 >> y2;
 
@@ -122,9 +122,9 @@ double a = abs(x2 - x1);          //вычисление стороны а
 
 double b = abs(y2 - y1);          //вычисление стороны в
 
-cout << "P = " ;2 * (a + b) << endl;      //вычисление периметра (прямоуг)
+cout << "P = " <<2 * (a + b) << endl;      //вычисление периметра (прямоуг)
 
-cout << "S = " ;a * b << endl;             //вычисление площади  (прямоуг)
+cout << "S = " <<a * b << endl;             //вычисление площади  (прямоуг)
 
 return 0;
 
@@ -156,7 +156,7 @@ int main()
 
 float x1, x2, x3, y1, y2, y3, a, b, c, p;
 
-cout<<"Enter the coordinates of the points (х1, у1), (х2, у2), (x3, y3) of triangle;
+cout<<"Enter the coordinates of the points (х1, у1), (х2, у2), (x3, y3) of triangle<<endl;
 
 cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
 
@@ -180,9 +180,9 @@ else
 
  p = (a + b + c) / 2;        //полупериметр
 
- cout << "P = "; p * 2 << endl;          //вывод периметра
+ cout << "P = "<< p * 2 << endl;          //вывод периметра
 
- cout << "S = "; sqrt(p * (p - a) * (p - b) * (p - c)) << endl;         //формула герона, вычисление площади треугольника
+ cout << "S = "<< sqrt(p * (p - a) * (p - b) * (p - c)) << endl;         //формула герона, вычисление площади треугольника
 
 }
 
